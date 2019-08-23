bfeb708c254709b1978f5ea55d8e4e0ec5284e5d
Leana <leananeparidze.com>
2019-08-23 10:24:41 -0700

    adds POST functionality to add a new color

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -8,7 +8,9 @@ const initialColor = {
 
 const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
+  const [adding, setAdding] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -30,6 +32,7 @@ const ColorList = ({ colors, updateColors }) => {
         updateColors(updatedList);
       })
       .catch(err => console.log(err.response));
+    setEditing(false);
   };
 
   const deleteColor = color => {
@@ -42,12 +45,24 @@ const ColorList = ({ colors, updateColors }) => {
       .catch(err => console.log(err.response));
   };
 
+  const addColor = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post(`http://localhost:5000/api/colors`, colorToAdd)
+      .then(res => {
+        updateColors(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }
+
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
         {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
+          <li key={color.color} onClick={() => {
+                                          if (adding) setAdding(false);
+                                          editColor(color)}}>
             <span>
               <span className="delete" onClick={() => deleteColor(color)}>
                 x
@@ -61,6 +76,9 @@ const ColorList = ({ colors, updateColors }) => {
           </li>
         ))}
       </ul>
+      <button onClick={() => {
+        if (editing) setEditing(false);
+        setAdding(true)}} style={{display: 'block', marginLeft: '40px'}}>+ add</button>
       {editing && (
         <form onSubmit={saveEdit}>
           <legend>edit color</legend>
@@ -91,6 +109,36 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
+      {adding && 
+        <form onSubmit={addColor}>
+            <legend>add color</legend>
+            <label>
+              color name:
+              <input
+                onChange={e =>
+                  setColorToAdd({ ...colorToAdd, color: e.target.value })
+                }
+                value={colorToAdd.color}
+              />
+            </label>
+            <label>
+              hex code:
+              <input
+                onChange={e =>
+                  setColorToAdd({
+                    ...colorToAdd,
+                    code: { hex: e.target.value }
+                  })
+                }
+                value={colorToAdd.code.hex}
+              />
+            </label>
+            <div className="button-row">
+              <button type="submit">add</button>
+              <button onClick={() => setEditing(false)}>cancel</button>
+            </div>
+        </form>
+       }
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
     </div>
